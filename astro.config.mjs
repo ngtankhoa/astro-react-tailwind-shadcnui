4901b316c3f23e69b2a31b6298474ef63ca2bc41
ngtankhoa <40681049+ngtankhoa@users.noreply.github.com>
2025-02-04 00:21:23 +0700

    remove tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+})
